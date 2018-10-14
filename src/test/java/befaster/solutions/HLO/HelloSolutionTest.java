741235a953846d648a789f4cfa2e3dcfff945245
sbx_user1060 <sbx_user1060@ip-10-129-104-160.eu-west-2.compute.internal>
2018-10-14 12:16:24 +0000

    Sun Oct 14 12:16:24 UTC 2018

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,4 +1,28 @@
 package befaster.solutions.HLO;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class HelloSolutionTest {
+    private HelloSolution helloSolution;
+
+    @Before
+    public void setUp() {
+        helloSolution = new HelloSolution();
+    }
+
+    @After
+    public void tearDown() {
+        helloSolution = null;
+    }
+
+    @Test
+    public void should_Return_Hello() {
+        String expected = "Hello";
+        String actual = helloSolution.hello("");
+        assertEquals(expected, actual);
+    }
 }

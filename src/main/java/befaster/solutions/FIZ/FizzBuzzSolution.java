9151fff4ad729f77c8dba3dcc06d8629f88ae60b
sbx_user1060 <sbx_user1060@ip-10-129-104-160.eu-west-2.compute.internal>
2018-10-14 12:27:42 +0000

    Sun Oct 14 12:27:42 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,6 +5,12 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
+        if (number % 3 == 0)
+            return "fizz";
+
+        if (number % 5 == 0)
+            return "buzz";
+
         throw new SolutionNotImplementedException();
     }
 
--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -1,4 +1,35 @@
 package befaster.solutions.FIZ;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class FizzBuzzSolutionTest {
+    private FizzBuzzSolution fizzBuzzSolution;
+
+    @Before
+    public void setUp() {
+        fizzBuzzSolution = new FizzBuzzSolution();
+    }
+
+    @After
+    public void tearDown() {
+        fizzBuzzSolution = null;
+    }
+
+    @Test
+    public void should_Return_fizz_When_Divisible_Only_By_3() {
+        assertEquals("fizz", fizzBuzzSolution.fizzBuzz(3));
+        assertEquals("fizz", fizzBuzzSolution.fizzBuzz(9));
+        assertEquals("fizz", fizzBuzzSolution.fizzBuzz(18));
+    }
+
+    @Test
+    public void should_Return_fizz_When_Divisible_Only_By_5() {
+        assertEquals("buzz", fizzBuzzSolution.fizzBuzz(5));
+        assertEquals("buzz", fizzBuzzSolution.fizzBuzz(25));
+        assertEquals("buzz", fizzBuzzSolution.fizzBuzz(5000));
+    }
 }
